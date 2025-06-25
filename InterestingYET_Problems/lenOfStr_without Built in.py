b5bf751b2a151937be204fbf8162120727245d6b
NavadeepDJ <142133431+NavadeepDj@users.noreply.github.com>
2025-06-25 14:26:15 +0530

    Create lenOfStr_without Built in.py

---
+++ InterestingYET_Problems/lenOfStr_without Built in.py
@@ -0,0 +1,15 @@
+#Slicing
+count1 =0
+str1 = input("")
+while str1:
+    print(str1)
+    str1 = str1[1:]
+    count1 = count1 + 1
+    
+print(count1)
+
+#for each type loop
+count2=0
+for i in str1:
+  count2 = count2 + 1
+print(count2)
